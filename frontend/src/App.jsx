14654c6e08d150691fc9889d5c406b1cd0d5a98f
Julian Lim Jun Ren <limjunre@bu.edu>
2024-11-12 18:49:36 -0500

    first couple components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar/navbar'
+import Profile from './components/profile/profile'
+import './App.css'
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Profile/>
+    </>
+  )
+}
+
+export default App
