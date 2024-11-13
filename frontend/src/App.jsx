b20497f4878f89de72033aa7be2dbec54fc30113
Julian Lim Jun Ren <limjunre@bu.edu>
2024-11-12 20:45:22 -0500

    created react components for about, projects, and experiences sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import Navbar from './components/navbar/navbar'
 import Profile from './components/profile/profile'
+import Projects from './components/projects/projects'
+import Experiences from './components/experiences/experiences'
+import About from './components/about/about'
 import './App.css'
 function App() {
   return (
     <>
       <Navbar/>
       <Profile/>
+      <About/>
+      <div id="projects">
+        <h2>Projects</h2>
+        <Projects name="Project 1" description="Project 1 Description" github="https://github.com"/>
+        <Projects name="Project 2" description="Project 2 Description" github="https://github.com"/>
+      </div>
+      <div id="exp">
+        <h2>Experiences</h2>
+        <Experiences title="Experience 1" info="Experience 1 Info" dates="Experience 1 Dates" bullets={["Bullet 1", "Bullet 2", "Bullet 3"]}/>
+        <Experiences title="Experience 2" info="Experience 2 Info" dates="Experience 2 Dates" bullets={["Bullet 1", "Bullet 2", "Bullet 3"]}/>
+      </div>
     </>
   )
 }
