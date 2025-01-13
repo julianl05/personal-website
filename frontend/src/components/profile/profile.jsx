6ed638dd0753da5f5ae728c6f6b03469e371345a
Julian Lim <limjunre@bu.edu>
2025-01-13 22:55:20 +0800

    added profile pic, modified color palette

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -3,10 +3,10 @@ import ProfileIcon from '../../assets/profile-icon.png'
 function Profile() {
     return (
         <div id="profile">
-                <div><img width="250" src={ProfileIcon} /></div>
+                <div><img id="profile-pic" width="250" src={ProfileIcon} /></div>
                 <div id="profile-text">
                 <p>Hi! I'm</p>
-                <h2>Julian Lim Jun Ren</h2>
+                <h2>Julian Lim</h2>
                 <p>Boston University Student and Aspiring SWE</p>
                 <div class="icons">
                     <a target="_blank" href="https://www.linkedin.com/in/julian-lim-jun-ren/"><img id="linkedin-img" width="60" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" /></a>
