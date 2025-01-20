9631f248d5309d1a42ea232dae3de0a69d78558f
Julian Lim <limjunre@bu.edu>
2025-01-19 22:13:06 -0500

    filled out bulletpoints for projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,8 +19,15 @@ function App() {
       </div>
       <div id="exp">
         <h2>Experiences</h2>
-        <Experiences title="Signify" info="Software R&D Intern" dates="June 2024 - August 2024" bullets={["Bullet 1", "Bullet 2", "Bullet 3"]}/>
-        <Experiences title="NYU Shanghai" info="Apprentice Programer" dates="June 2021 - August 2021" bullets={["Bullet 1", "Bullet 2", "Bullet 3"]}/>
+        <Experiences title="Signify" info="Software R&D Intern" dates="June 2024 - August 2024" 
+          bullets={["Served as a team member of a R&D project team specializing in improving customer experience for large scale installations (~100) of IoT smart LED lights. ",
+                    "Wrote Javascript and Python scripts for MQTT.fx, Apache JMETER, and Locust that automated MQTT, UDP, and ESP-Now connectivity testing for lights controlled with the WiZ Connected mobile app. ",
+                    "Developed a Python PyQT5 GUI for controlling rectangular grids of RGB WiZ lights, capable of displaying scrolling text, images, gifs, chinese characters, etc."]}/>
+
+        <Experiences title="NYU Shanghai" info="Apprentice Programer" dates="June 2021 - August 2021" 
+          bullets={["Contributed to a python-based search engine utilizing gRPC and bert_as_a_service during a span of 4 weeks.",
+                    "Incorporated elements of pattern recognition to teach search engines to present relevant results.", 
+                    "Navigated file systems through UNIX command line tools."]}/>
       </div>
     </>
   )
