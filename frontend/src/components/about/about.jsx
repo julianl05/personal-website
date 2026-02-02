e40bb0c614726ecfb0c4750fb5647d76fc79237d
Julian Lim Jun Ren <limjunre@bu.edu>
2026-02-02 16:48:18 -0500

    updated skills

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -2,9 +2,14 @@ import './about.css'
 import CSS from '../../assets/css.svg'
 import HTML from '../../assets/html.svg'
 import JS from '../../assets/js.svg'
+import TS from '../../assets/ts.svg'
 import PY from '../../assets/py.svg'
 import REACT from '../../assets/react.svg'
 import NODE from '../../assets/node.svg'
+import DJ from '../../assets/django.svg'
+import MONGODB from '../../assets/mongodb.svg'
+import POSTGRESQL from '../../assets/postgresql.svg'
+import JAVA from '../../assets/java.svg'
 import { useState } from 'react'
 
 function About() {
@@ -27,10 +32,10 @@ function About() {
             </h2>
             <div style={{display: display}}>
                 <p>
-                    Hi, welcome to my portfolio! I'm a senior studying CS at Boston University that's still figuring things out. Most recently, I've interned at Splunk, where I worked on automating E2E test deployment and shipping customer-facing features for their OT Intelligence app.
+                    Hi, welcome to my portfolio! I'm a senior studying CS at Boston University with a passion for developing full-stack web applications. Most recently, I've interned at Splunk, where I worked on automating E2E test deployment and shipping customer-facing features for their OT Intelligence app.
                     My personal interests range from binging high fantasy novels like the Stormlight Archive series, 
                     to playing cozy farming games like Stardew Valley and sinking unfathomable hours into Rocket League, 
-                    so feel free to talk to me about anything nerdy, cause chances are I probably dabble in it :) Contact me at julianlim05@gmail.com.
+                    so feel free to talk to me about anything nerdy, cause chances are I probably dabble in it :) Contact me at limjunre@bu.edu.
                 </p>
                 <br />
                 <h3>My Skills</h3>
@@ -38,9 +43,14 @@ function About() {
                     <img width="60" src={HTML} />
                     <img width="60" src={CSS} />
                     <img width="60" src={PY} />
+                    <img width="60" src={JAVA} />
                     <img width="60" src={JS} />
+                    <img width="60" src={TS} />
                     <img width="60" src={REACT} />
                     <img width="60" src={NODE} />
+                    <img width="60" src={DJ} />
+                    <img width="60" src={MONGODB} />
+                    <img width="60" src={POSTGRESQL} />
                 </div>
             </div>
         </div>
