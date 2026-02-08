cd9f903ba74b3e87ee4550e0af5b5ca16ac5238e
Julian Lim Jun Ren <limjunre@bu.edu>
2026-02-07 19:32:25 -0500

    separated navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -10,18 +10,16 @@ function Navbar() {
 
     return (
         <div id="navbar">
-            <div className="navbar-content">
-                <h1>Julian Lim Jun Ren</h1>
-                <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-                <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-                    <a href="#about" onClick={() => setIsOpen(false)}>About</a>
-                    <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
-                    <a href="#exp" onClick={() => setIsOpen(false)}>Experiences</a>
-                </div>
+            <h1>Julian Lim Jun Ren</h1>
+            <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <a href="#about" onClick={() => setIsOpen(false)}>About</a>
+                <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
+                <a href="#exp" onClick={() => setIsOpen(false)}>Experiences</a>
             </div>
         </div>
     )
